c3c5110b1d0992f1ad99e887af720b40b518ac93
JaceParks <parkjace@oregonstate.edu>
2021-05-31 22:51:39 -0700

    fixed mistake

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def pytest():
     assert q1.fizz_buzz(3) == 'Fizz'
     assert q1.fizz_buzz(5) == 'Buzz'
     assert q1.fizz_buzz(15) == 'FizzBuzz'
-    assert q1.fizz_buzz(30) == 'Fizz'
+    assert q1.fizz_buzz(30) == 'FizzBuzz'
     assert q1.fizz_buzz(55) == 'Buzz'
 
 
