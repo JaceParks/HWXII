2f83c0e52e5f017b3d079f8ef9bdadea73e3e2ae
JaceParks <parkjace@oregonstate.edu>
2021-05-31 23:05:24 -0700

    4th test

--- testleap.py
+++ testleap.py
@@ -7,8 +7,9 @@ def pytest():
     #pytest test
     assert q2.again(4) == True
     assert q2.again(100) == False
-    assert q2.again(400) == False
-    
+    assert q2.again(400) == True
+    assert q2.again(504) == True
+
 print("----------PYTEST----------")
 pytest()
 print("----------END TEST--------")
