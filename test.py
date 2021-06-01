bfcefd07d9b387c4b1b7e73cab5dd5e3006de52b
JaceParks <parkjace@oregonstate.edu>
2021-05-31 22:39:08 -0700

    first test

---
+++ test.py
@@ -0,0 +1,13 @@
+import pytest
+import unittest
+import leapyearWOE
+import sys
+
+def pytest():
+    #pytest test
+    assert q1.fizz_buzz(3) == 'Fizz'
+    
+
+print("----------PYTEST----------")
+pytest()
+print("----------END TEST--------")
