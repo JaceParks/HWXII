2765d297a80b6cf115f97fd5ed2a32c6726b3569
JaceParks <parkjace@oregonstate.edu>
2021-05-31 22:44:25 -0700

    5th

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ def pytest():
     assert q1.fizz_buzz(5) == 'Buzz'
     assert q1.fizz_buzz(15) == 'FizzBuzz'
     assert q1.fizz_buzz(30) == 'Fizz'
+    assert q1.fizz_buzz(55) == 'Buzz'
+
 
 print("----------PYTEST----------")
 pytest()
